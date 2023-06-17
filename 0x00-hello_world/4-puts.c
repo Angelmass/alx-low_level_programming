74d180e2dff1c2f686b289cb2258741c9465091d
Angel Maseko <angellethabmaseko@gmail.com>
2023-06-17 12:08:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts functio
+ n
+ *Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzl
+e");
+return (0);
+}
